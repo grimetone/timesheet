bf3108f5b425dbd43d75aaf3ba2318dd6d08429f
sean <seanaltoft@gmail.com>
2018-12-06 06:23:21 +0100

    feat: Add basic query and mutation to test graphql

--- core/src/resolvers/mutation.js
+++ core/src/resolvers/mutation.js
@@ -1,3 +1,20 @@
-const Mutation = {};
+const Mutation = {
+  // Creates a user
+  async createUser(parent, args, ctx, info) {
+    const user = await ctx.db.mutation.createAccount(
+      {
+        data: {
+          ...args
+        },
+      },
+      info
+    );
+   return user;
+  },
+  createTimesheet(parent, args, ctx, info) {},
+  createWorkPeriod(parent, args, ctx, info) {},
+  createProject(parent, args, ctx, info) {},
+  createExpense(parent, args, ctx, info) {}
+};
 
 module.exports = Mutation;
--- core/src/resolvers/query.js
+++ core/src/resolvers/query.js
@@ -1,3 +1,8 @@
-const Query = {};
+const Query = {
+  // Returns all users(accounts)
+  async users(parent, args, ctx, info) {
+    return ctx.db.query.accounts({}, info);
+  }
+};
 
 module.exports = Query;
