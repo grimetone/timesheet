099d7bfc3e89c20fae3ad7773ac24003656eb5ce
sean <seanaltoft@gmail.com>
2018-12-22 02:19:00 +0100

    fix: Fix incorrect db query

--- core/src/resolvers/query.js
+++ core/src/resolvers/query.js
@@ -6,7 +6,7 @@ const Query = {
     if (!ctx.request.userId) {
       return null;
     }
-    return ctx.db.query.user({ where: { id: ctx.request.userId } }, info);
+    return ctx.db.query.account({ where: { id: ctx.request.userId } }, info);
   },
   // Returns a user
   async user(parent, args, ctx, info){
